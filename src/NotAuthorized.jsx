a6c24761e2faf7af7114034e3fd08f8ec4d8a2d7
Subodh Kulkarni <126555650+iamSubodh@users.noreply.github.com>
2025-04-30 10:42:35 +0530

    Delete src directory

--- src/NotAuthorized.jsx
+++
@@ -1,4 +0,0 @@
-export default function NotAuthorized() {
-    return <h2>403 - You are not authorized to access this page</h2>;
-  }
-  
--- src/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from './AuthContext';
-import { Navigate } from 'react-router-dom';
-
-export function PrivateRoute({ children, roleRequired }) {
-  const { user } = useAuth();
-  if (!user) return <Navigate to="/login" />;
-  if (roleRequired && user.role !== roleRequired) return <Navigate to="/not-authorized" />;
-  return children;
-}
